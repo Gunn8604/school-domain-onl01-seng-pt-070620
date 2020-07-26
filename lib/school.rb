2960f77bdf67e197da54586e9af21f1760b62589
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-07-26 15:00:15 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,15 +2,14 @@ class School
   attr_accessor :grade, :roster
   attr_reader :name
   
-  roster["grade"] = []
-  roster["grade"] << "name"
-  roster {"grade"=> ["name"]}
   def initialize(name)
     @name = name
     @roster = {}
   end
   
- 
+  roster["grade"] = []
+  roster["grade"] << "name"
+  roster {"grade"=> ["name"]}
   
   def add_student
     @roster.push("#{name}, #{grade}")
