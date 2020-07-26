8a2ea328dcc8d4d518b6494c2087386dbdf08e07
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-07-26 15:55:32 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,8 @@ class School
 
   
   def add_student(name, grade)
-    if roster[grade] << name
+    if roster[grade]
+      << name
     else
       roster[grade] = []
       #roster {[grade, name]}
